f1d4f2ab8987f2d0702ce24bfbef4608b02a4b67
danielmoessner <github@danielmoessner.de>
2024-12-24 11:05:18 +0100

    add text

---
+++ pull.sh
@@ -0,0 +1,2 @@
+scp root@qc-quality-control.de:/home/qc-quality-control.de/tmp/db.sqlite3 ./tmp/db.sqlite3
+scp root@qc-quality-control.de:/home/qc-quality-control.de/tmp/secrets.json ./tmp/secrets.json
